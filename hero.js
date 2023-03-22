63e568071bcc23f8ae2fb07f0c551d1d199dd96d
beepek <ben.schylling@gmail.com>
2023-03-22 13:01:18 -0400

    more logic work on controller game and display

--- hero.js
+++ hero.js
@@ -1,9 +1,25 @@
 
 
-var controller = new Controller();
+let controller = new Controller();
+let display = new Display(document.querySelector("canvas"));
+let game = new Game();
+let engine = new Engine(1000/30, render, update);
 
-var display = new Display(document.querySelector("canvas"));
+const update= function() {
+    if (controller.left.active)  {game.world.player.moveLeft(); }
+    if (controller.right.active) {game.world.player.moveRight();}
+    if (controller.up.active) {game.world.player.jump(); controller.up.active = false;}
+    game.update();
+};
 
-var game = new Game();
 
-var engine = new Engine(1000/30, render, update);
+display.buffer.canvas.height = game.world.height;
+display.buffer.canvas.width = game.world.width;
+
+window.addEventListener("keydown", keyDownUp);
+window.addEventListener("keyup", keyDownUp);
+window.addEventListener("resize", resize);
+
+resize();
+
+engine.start();
